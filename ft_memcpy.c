4c16e25ce717fb3e6a272e4c291cc9a56db1fd0e
Tsubasa Ishihara <tishihar@c2r4s4.42tokyo.jp>
2024-05-13 23:22:15 +0900

    0513

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 05:33:39 by tishihar          #+#    #+#             */
-/*   Updated: 2024/05/13 21:20:48 by tishihar         ###   ########.fr       */
+/*   Updated: 2024/05/13 23:20:29 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
 	const unsigned char *s = (const unsigned char *)src;
 	unsigned char *d = (unsigned char *)dst;
 
+	if (!dst && !src)
+		return (NULL);
+
 	while(n--)
 	{
 		*d = *s;
@@ -25,4 +28,3 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
 	}
 	return (dst);
 }
-
