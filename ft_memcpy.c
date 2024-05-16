f4b4ee6aaa911388ee901389e6e3b08bcb3e67b3
Anya-Stella <wingstonetone9.8@gmail.com>
2024-05-16 20:27:46 +0900

    🔥 初commit

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tishihar <tishihar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: tishihar <wingstonetone9.8@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 05:33:39 by tishihar          #+#    #+#             */
-/*   Updated: 2024/05/14 17:54:23 by tishihar         ###   ########.fr       */
+/*   Updated: 2024/05/16 15:09:16 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,23 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	}
 	return (dst);
 }
+
+#include <stdio.h>
+
+int main(void)
+{
+	char *src1 = "helloooooo,wooooorlds!!";
+	printf("%s : this is a src1\n", src1);
+
+	// 完全一致の場合
+	ft_memcpy(src1, src1, 2);
+
+	printf("%s : this is a src1 re\n", src1);
+
+
+
+
+
+	return (0);
+}
+
