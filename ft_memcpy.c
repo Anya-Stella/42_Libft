18e6b7089a91878f5b6c886ed6cc5e6f57a6f197
Tsubasa Ishihara <tishihar@c2r4s4.42tokyo.jp>
2024-05-18 16:02:04 +0900

    memmove

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 05:33:39 by tishihar          #+#    #+#             */
-/*   Updated: 2024/05/14 17:54:23 by tishihar         ###   ########.fr       */
+/*   Updated: 2024/05/18 15:41:08 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,49 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	if (!dst && !src)
 		return (NULL);
 	while (n--)
-	{
-		*d = *s;
-		s++;
-		d++;
-	}
+		*(d++) = *(s++);
 	return (dst);
 }
+
+/*
+// memcpyはmemmoveの下位互換
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	// print test
+	char *dst1 = "helllllo!!!woooooorld!!";
+	printf("%s: this is a origin\n", dst1);
+	fflush(stdout);
+
+
+
+	//dst < src
+	char dst2[] ="helllllo!!!woooooorld!!";
+	ft_memcpy(dst2, dst2+3, 10);
+	printf("%s: this is a dst2\n", dst2);
+	
+	//dst > src
+	char dst3[] ="helllllo!!!woooooorld!!";
+	ft_memcpy(dst3+3, dst3, 10);
+	printf("%s: this is a dst3: mine_memcpy\n", dst3);
+
+	//dst > src
+	// char dst4[] ="helllllo!!!woooooorld!!";
+	// memcpy(dst4+3, dst4, 10);
+	// printf("%s: this is a dst4: origin_memcpy\n", dst4);
+
+	//memmove
+
+	//dst > src
+	char dst5[] ="helllllo!!!woooooorld!!";
+	memmove(dst5+3, dst5, 10);
+	printf("%s: this is a dst5: memmove\n", dst5);
+
+
+
+
+	return (0);
+}
+
+//*/
