7c648dd01d27b62a0be821d821263f0aa7cc1419
Tsubasa Ishihara <tishihar@c1r1s7.42tokyo.jp>
2024-05-20 23:18:57 +0900

    strnstr

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,29 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tishihar <wingstonetone9.8@gmail.com>      +#+  +:+       +#+        */
+/*   By: tishihar <tishihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 17:19:13 by tishihar          #+#    #+#             */
-/*   Updated: 2024/05/19 17:48:57 by tishihar         ###   ########.fr       */
+/*   Updated: 2024/05/20 23:13:29 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    unsigned char   *c1;
-    unsigned char   *c2;
+	unsigned char	*c1;
+	unsigned char	*c2;
 
-    c1 = (unsigned char *)s1;
-    c2 = (unsigned char *)s2;
-
-    while (n-- && (*c1 || *c2))
-    {
-        if (*c1 != *c2)
-            return (*c1 - *c2);
-        c1++;
-        c2++;
-    }
-    return (0);
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
+	while (n-- && (*c1 || *c2))
+	{
+		if (*c1 != *c2)
+			return (*c1 - *c2);
+		c1++;
+		c2++;
+	}
+	return (0);
 }
